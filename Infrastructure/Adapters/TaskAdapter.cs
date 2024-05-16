fca9ba556dd2fbb07e73022a6e8544e727f232eb
BitawareUnleashed <dario.benevento@gmail.com>
2024-05-16 22:03:45 +0200

    Initial commit

---
+++ Infrastructure/Adapters/TaskAdapter.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using Infrastructure.Interfaces;
+namespace Infrastructure.Adapters;
+public class TaskAdapter : ITaskPort
+{
+    private static List<TaskModel> tasks = new List<TaskModel>();
+    public IEnumerable<TaskModel> GetAllTasks() => tasks;
+    public void AddTask(TaskModel task) => tasks.Add(task);
+    public void UpdateTask(TaskModel task)
+    {
+        if (tasks.Contains(task))
+        {
+            var updateTask = tasks.Where(x => x.Id == task.Id).FirstOrDefault();
+            updateTask = task;
+        }
+    }
+    public void DeleteTask(int taskId)
+    {
+        var task = tasks.Where(x => x.Id == taskId).FirstOrDefault();
+        if (task is not null)
+        {
+            tasks.Remove(task);
+        }
+    }
+}
